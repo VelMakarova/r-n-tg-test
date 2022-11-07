6b49d339443bd24905d8b11df2abfafb276ebc66
vel-mak <valeria.v.makarova@gmail.com>
2022-11-07 18:03:46 +0200

    upd1

--- src/App.js
+++ src/App.js
@@ -10,12 +10,14 @@ function App() {
   }
 
   useEffect(() => {
+    console.log(webapp.initDataUnsafe?.user)
     webapp.ready()
   }, [])
   return (
     <div className="app">
-      <h1 className="title">works</h1>
+      <h1 className="title">App</h1>
       <button onClick={onClose}>Close</button>
+      <span>{webapp.initDataUnsafe?.user?.username}</span>
     </div>
   );
 }
